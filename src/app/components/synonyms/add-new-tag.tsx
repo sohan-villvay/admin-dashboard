d72c4e25e7a896c03f3d72538f01d04f6ac12ed6
sohan-villvay <sohan@villvay.com>
2024-02-17 19:14:42 +0530

    Fixed button issue in add-new-tag

--- src/app/components/synonyms/add-new-tag.tsx
+++ src/app/components/synonyms/add-new-tag.tsx
@@ -1,19 +1,23 @@
 import { CheckIcon } from "@radix-ui/react-icons"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { IconButton } from "@radix-ui/themes";
 
 export function AddNewTag() {
   return (
 
     <div className="flex flex-row m-2 group">
+
       <Input placeholder="+" className="rounded-full h-7 w-7 transition-all ease-in-out focus:w-36 duration-300
-       p-2 focus:placeholder-transparent" />
-      <Button className="h-7 w-7 rounded-full bg-green-500 ml-1 invisible">
+       p-2 focus:placeholder-transparent group" />
+
+      <Button className="h-7 w-7 rounded-full bg-lime-600/50 ml-1 p-2 invisible group-focus-within:visible">
+
         <div className="text-white">
           <CheckIcon />
         </div>
+
       </Button>
+
     </div>
   )
 }
